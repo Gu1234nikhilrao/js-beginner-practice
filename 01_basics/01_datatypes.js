7dd8c7ac2528e381da44cc541cf0cd122e61fbe6
Gu1234nikhilrao <s.23scse1460015@galgotiasuniversity.ac.in>
2025-04-23 10:06:18 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all jS code as newer virsion
+
+// alert( 4+4) //we are using nodejs not browser 
+
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+//udefined =>
+//symbol => unique
+
+
+// object 
+
+console.log(typeof undifined); // undifine
+console.log(typeof null); // object
+
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 1345 
+let accountEmail = "snikhilrao@gmai.com"
+var accountPassword = "54321"
+accountCity = "delhi"
+let accountstate 
+//accountId = 2 /not allowed 
+
+accountEmail = "vinnu@gmail"
+accountPassword="1434"
+accountCity= "hydrabad"
+console.log(accountId)
+/* 
+prefer not to use var
+because of issue in block scope and function scope
+*/
+console.table([accountEmail ,accountPassword,accountCity,accountstate])
+
